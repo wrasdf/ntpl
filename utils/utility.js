4a1d064c4ee566e92120c514bc819d81a9a53e61
kerry.wang <wrasdf@gmail.com>
2018-02-12 10:21:50 +1100

    WIP

---
+++ utils/utility.js
@@ -0,0 +1,27 @@
+
+const fs = require('fs'),
+    path = require('path'),
+    yaml = require('js-yaml');
+
+function getFileExtensionName(filePath) {
+  return path.extname(filePath).replace('\.', '').toLowerCase();
+}
+
+function isInSupportList(filePath, list) {
+  if (list.indexOf(getFileExtensionName(filePath)) < 0) {
+    throw new Error(`Only support ${list}`)
+  }
+}
+
+function paramsReader(filePath) {
+  if (getFileExtensionName(filePath) == 'json'){
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'))
+  }
+  return yaml.load(fs.readFileSync(filePath, 'utf8'));
+}
+
+module.exports = {
+  isInSupportList: isInSupportList,
+  getFileExtensionName: getFileExtensionName,
+  paramsReader: paramsReader
+}
