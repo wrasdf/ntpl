9c3be53d08b6468d437c752dd71e4e7340cf1236
kerry.wang <wrasdf@gmail.com>
2018-02-12 21:08:15 +1100

    fix function test always pass issue

--- tests/function-tests/function-test.js
+++ tests/function-tests/function-test.js
@@ -3,28 +3,31 @@ const expect = require('chai').expect,
 
 describe("utility", ()=>{
 
-  it("should generate correct template based on yaml", ()=>{
+  it("should generate correct template based on yaml", (done)=>{
     let bashScript = './ntpl -t ./tests/function-tests/template.yaml -p ./tests/function-tests/params.yaml'
     exec(bashScript, function(err, stdout, stderr) {
-      expect(stdout).have.string('name: "kube-demo"');
-      expect(stdout).have.string('namespace: "kube-demo"');
+      expect(stdout).have.string('name: "kube-demo"')
+      expect(stdout).have.string('namespace: "kube-demo"')
+      done()
     });
   })
 
-  it("should generate correct template based on json", ()=>{
+  it("should generate correct template based on json", (done)=>{
     let bashScript = './ntpl -t ./tests/function-tests/template.yaml -p ./tests/function-tests/params.json'
     exec(bashScript, function(err, stdout, stderr) {
-      expect(stdout).have.string('name: "kube-demo"');
-      expect(stdout).have.string('app: "kube-demo"');
+      expect(stdout).have.string('name: "kube-demo"')
+      expect(stdout).have.string('app: "kube-demo"')
+      done()
     });
   })
 
-  it("should generate correct template based on environment variables", ()=> {
+  it("should generate correct template based on environment variables", (done)=> {
     process.env.USERNAME = "Kerry"
     let bashScript = './ntpl -t ./tests/function-tests/template.yaml -p ./tests/function-tests/params.json -e USERNAME'
     exec(bashScript, function(err, stdout, stderr) {
-      expect(stdout).have.string('name: "kube-demo"');
-      expect(stdout).have.string('USERNAME: "Kerry"');
+      expect(stdout).have.string('name: "kube-demo"')
+      expect(stdout).have.string('env: "Kerry"')
+      done()
     });
   })
 
