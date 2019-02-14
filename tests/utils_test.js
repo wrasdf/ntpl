a36a2cdd2d1fbc65c3141a9bdc48317a0f727ae0
kerry.wang <wrasdf@gmail.com>
2019-02-14 17:42:52 +1100

    clean code

--- tests/utils_test.js
+++ tests/utils_test.js
@@ -13,23 +13,4 @@ describe('utils functions', () => {
     });
   })
 
-  // describe('getFoldersByFiles', () => {
-  //   it(`should return all folders`, async () => {
-  //     const files = [
-  //       '/app/test1.yaml',
-  //       '/app/test2/a.yaml',
-  //       '/app/test2/b.yaml',
-  //       '/app/test3/a.yaml',
-  //       '/app/test3/c.yaml',
-  //       '/app/test3/test4/a.yml'
-  //     ]
-  //     // const objects = utils.createdirs(`${__dirname}/files`, `${__dirname}/_build`)
-  //     expect(utils.getFoldersByFiles(files)).to.eql([
-  //       '/app',
-  //       '/app/test2',
-  //       '/app/test3',
-  //       '/app/test3/test4'
-  //     ])
-  //   });
-  // })
 });
--- utils/utility.js
+++ utils/utility.js
@@ -25,32 +25,6 @@ function readdir(dir) {
   return fs.readdirSync(dir).map(f => join(dir, f))
 }
 
-function rreaddir(dir, allFiles = []) {
-  const files = fs.readdirSync(dir).map(f => join(dir, f))
-  allFiles.push(...files)
-  files.forEach(f => {
-    fs.statSync(f).isDirectory() && readdir(f, allFiles)
-  })
-  return allFiles
-}
-
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index
-}
-
-function getFoldersByFiles(files, dir, dest) {
-  return files.map(file => file.split('/').slice(0, -1).join('/'))
-    .filter(onlyUnique)
-    .map(folder => folder.replace(new RegExp(dir), `${dest}`))
-}
-
-function createdirs(dir, destination) {
-  rmdir(destination)
-  let folders = getFoldersByFiles(readdir(dir))
-    .map(folder => folder.replace(new RegExp(dir), `${destination}`))
-    .map(folder => mkdir(`${folder}`))
-}
-
 function readfile(path) {
   return fs.readFileSync(path, 'utf8');
 }
@@ -61,12 +35,10 @@ function appendFile(path, content) {
 
 module.exports = {
   yamlPaser,
-  appendFile,
   exec,
   mkdir,
   rmdir,
   readdir,
   readfile,
-  getFoldersByFiles,
-  createdirs
+  appendFile
 }
